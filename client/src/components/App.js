e88b9f1f4c56162dc82adf837edcb0994b6a3279
Douglas Hellberg <DougHellberg@gmail.com>
2023-10-18 15:59:56 -0400

    Created CSS file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React, { useEffect, useState } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -17,7 +17,7 @@ function PostCard({address, price, house_img, num_of_baths, num_of_beds, descrip
             <p>Beds: {num_of_beds}</p>
             <p>Baths: {num_of_baths}</p>
             <p>Sqft: {square_feet}</p>
-            <p>description: {description}</p>
+            <p>Description: {description}</p>
             <h5>Lister: {user}</h5>
         </div>
     </>)
