5d2ddd83cf21e9b8076ffe954f4e6378f417e411
Baileyb95 <108492331+Baileyb95@users.noreply.github.com>
2023-10-18 15:29:24 -0400

    updated favorties

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import {BsSuitHeart, BsSuitHeartFill} from "react-icons/bs";
 
 function PostCard({address, price, house_img, num_of_baths, num_of_beds, description, square_feet, user}) {
+   const [favorite, setIsFavorite] = useState(true);
+
+   function handleClick() {
+       setIsFavorite(!favorite);
+   }
+   const favoriteHouse = favorite ? <BsSuitHeartFill onClick={handleClick} /> : <BsSuitHeart onClick={handleClick} />;
     return (<>
         <div>
+            <span className="favorite">{favoriteHouse}</span>
             <img src={house_img} alt={address}></img>
             <h4>{address}</h4>
             <p>Price: {price}</p>
