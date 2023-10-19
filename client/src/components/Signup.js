6818473d645325367b105b3f961f5d6e70bf9289
Eman Gurung <egurung1117@gmail.com>
2023-10-19 16:10:00 -0400

    added sign up form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,8 +2,23 @@
 
 
 function Signup() {
+
+    
+
     return <>
-        Sign up
+        <div>
+            <form>
+                <label>Username</label>
+                <input name="name" placeholder="username" type="text"></input>
+                <label>Password</label>
+                <input name="password" type="password" placeholder="password"></input>
+                <label>Email</label>
+                <input name="email" type="email" placeholder="email"></input>
+                <label>Phone number</label>
+                <input name="phone" type="text" placeholder="(xxx)-(xxx)-(xxxx)"></input>
+                <input type="submit" value="Sign up"></input>
+            </form>
+        </div>
     </>
 }
 
