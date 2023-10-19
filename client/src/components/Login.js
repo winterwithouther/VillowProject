255e9c77232546aa12405160b1f768b46e44609a
Eman Gurung <egurung1117@gmail.com>
2023-10-19 16:15:48 -0400

    added login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,11 @@
 
 function Login() {
     return <>
-        Login
+        <form>
+            <input type="text" name="name" placeholder="username"></input>
+            <input type="password" name="password" placeholder="password"></input>
+            <input type="submit" value="Log in"></input>
+        </form>
     </>
 }
 
